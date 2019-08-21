57b087e0e5e1ac2511f9a7adc3acfe8dedfdfd10
prometheus61 <btceeds@gmail.com>
2019-08-21 16:29:59 +0100

    add database connection

--- GelatoUI/CustomerForm.cs
+++ GelatoUI/CustomerForm.cs
@@ -16,14 +16,6 @@ namespace GelatoUI
         public CustomerForm()
         {
             InitializeComponent();
-            //ListBox listbox1 = new ListBox();
-            //listbox1.Location = new System.Drawing.Point(20, 20);
-            //listbox1.Name = "List from Code";
-            //listbox1.Size = new System.Drawing.Size(300, 300);
-            //listbox1.BackColor = Color.Green;
-            //listbox1.ForeColor = Color.White;
-            //listbox1.BorderStyle = BorderStyle.Fixed3D;
-            //Controls.Add(listbox1);
         }
 
         private void ClearButton_Click(object sender, EventArgs e)
@@ -47,26 +39,11 @@ namespace GelatoUI
 
         private void CustomerForm_Load(object sender, EventArgs e)
         {
-            string connectionString;
-            SqlConnection conn;
-            connectionString = @"Data Source =(localdb)\MSSQLLocalDB; Initial Catalog = master; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
-            conn = new SqlConnection(connectionString);
-            conn.Open();
-            MessageBox.Show("Connected to Database");
-            //    conn.Close();
 
-            SqlCommand command;
-            SqlDataReader dataReader;
-            String sql,Output = "";
+            Gelato2UEntities db = new Gelato2UEntities();
+            List<Customer> cl = db.Customers.ToList();
+            
 
-            sql = "Select CustomerName, CustomerNumber from dbo.Customers";
-            command = new SqlCommand(sql, conn);
-            dataReader = command.ExecuteReader();
-            while (dataReader.Read())
-            {
-                Output = Output + dataReader.GetValue(1) + "\n";
-            }
-            MessageBox.Show(Output);
 
         }
     }
