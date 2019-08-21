09f053108b342b0797d4ebe536f0dfa2d13b009e
prometheus61 <btceeds@gmail.com>
2019-08-21 13:48:18 +0100

    Add db connection to home page

--- GelatoUI/CustomerForm.cs
+++ GelatoUI/CustomerForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -42,6 +43,31 @@ namespace GelatoUI
             this.Hide();
             OrderBasketForm ob = new OrderBasketForm();
             ob.ShowDialog();
+        }
+
+        private void CustomerForm_Load(object sender, EventArgs e)
+        {
+            string connectionString;
+            SqlConnection conn;
+            connectionString = @"Data Source =(localdb)\MSSQLLocalDB; Initial Catalog = master; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+            conn = new SqlConnection(connectionString);
+            conn.Open();
+            MessageBox.Show("Connected to Database");
+            //    conn.Close();
+
+            SqlCommand command;
+            SqlDataReader dataReader;
+            String sql,Output = "";
+
+            sql = "Select CustomerName, CustomerNumber from dbo.Customers";
+            command = new SqlCommand(sql, conn);
+            dataReader = command.ExecuteReader();
+            while (dataReader.Read())
+            {
+                Output = Output + dataReader.GetValue(1) + "\n";
+            }
+            MessageBox.Show(Output);
+
         }
     }
 }
